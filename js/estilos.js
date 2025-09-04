e2f6ba4ac0e7129655a62ca27b5e1d20867aaaa1
Adriana Castillo <adricasra1993@gmail.com>
2025-09-04 09:48:20 -0500

    Creación de la function cambiar el fondo de la figura por un GIF animado

--- js/estilos.js
+++ js/estilos.js
@@ -8,8 +8,16 @@ function circulo() {
 
 function estrella(){
     figura.style.clipPath = 'polygon(50% 0%, 61% 35%,98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%,  39% 35% )';
+    figura.style.backgroundColor = 'green';
+}
+
+function fondoGif(){
+    figura.style.backgroundImage= "url('img/minions.gif')";
+    figura.style.backgroundSize= '100% 100%';
+    figura.style.backgroundRepeat= 'no-repeat';
 }
 
 
 document.getElementById('btnCircle').addEventListener('click', circulo);
 document.getElementById('btnStar').addEventListener('click', estrella);
+document.getElementById('btnGif').addEventListener('click', fondoGif);
