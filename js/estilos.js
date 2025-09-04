ef320ddef888d3a5a87c1c38d4e75771054c3ae4
Adriana Castillo <adricasra1993@gmail.com>
2025-09-04 09:26:38 -0500

    creación de la function para conviertir la figura en una estrella con clip-path

--- js/estilos.js
+++ js/estilos.js
@@ -6,4 +6,10 @@ function circulo() {
     figura.style.backgroundColor = 'orange';
 }
 
+function estrella(){
+    figura.style.clipPath = 'polygon(50% 0%, 61% 35%,98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%,  39% 35% )';
+}
+
+
 document.getElementById('btnCircle').addEventListener('click', circulo);
+document.getElementById('btnStar').addEventListener('click', estrella);
