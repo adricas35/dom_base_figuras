261b250a839b1c77ffd4f376f17d7adf69ea1b46
Adriana Castillo <adricasra1993@gmail.com>
2025-09-04 16:53:14 -0500

    Creación de la function para elegir la forma de la figura

--- js/atributos.js
+++ js/atributos.js
@@ -7,6 +7,25 @@ function cambiarColor(){
     figuraAtributos.style.backgroundColor= colorFigura;
 }
 
+function elegirFigura(){
+    const figura= prompt('Elige entre círculo, estrella o cuadrado');
+    switch (figura) {
+        case 'estrella':
+            figuraAtributos.style.clipPath = 'polygon(50% 0%, 61% 35%,98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%,  39% 35% )';            
+            break;
+
+        case 'círculo':
+            figuraAtributos.style.clipPath = 'circle(50%)';
+            figuraAtributos.style.backgroundColor = 'orange';
+            break;
+        case'cuadrado':
+            figuraAtributos.style.clipPath = 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)';
+            default:
+            break;
+    }  
+}
+
 
 
 document.querySelector('#btnHexColor').addEventListener('click', cambiarColor);
+document.querySelector('#btnChooseFigure').addEventListener('click', elegirFigura);
