19839f4d3d5d8283d6e3f6c0e8fb99d499b8cb17
Adriana Castillo <adricasra1993@gmail.com>
2025-09-04 14:59:00 -0500

    Creación de la function para cambiar el parrafo

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,10 +1,17 @@
 
 let titulo = document.querySelector('#mainTitle');
+let parrafo = document.querySelector('#mainParagraph');
 
 function cambiarTitulo(){
     const nuevoTitulo = prompt('Ingrese su nuevo titulo');
     titulo.innerText = nuevoTitulo;
 }
 
+function cambiarParrafo(){
+    const nuevoParrafo = prompt('Ingrese su nuevo parrafo');
+    parrafo.innerText = nuevoParrafo;
+}
+
 
 document.querySelector('#btnChangeTitle').addEventListener('click', cambiarTitulo);
+document.querySelector('#btnChangeParagraph').addEventListener('click', cambiarParrafo);
